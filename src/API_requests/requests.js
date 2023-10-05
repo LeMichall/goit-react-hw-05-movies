35afe1eacb948e989be74364b2e7bec63c4be5ca
Michal Leszczyk <Michal Leszczyk>
2023-10-05 13:22:27 +0200

    refactored request.js using axios

--- src/API_requests/requests.js
+++ src/API_requests/requests.js
@@ -1,37 +1,32 @@
+import axios from 'axios';
+
 const APIkey = '066dd3f638d42b734e86b50ba07e683c';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+axios.defaults.params = {
+  language: 'enUS',
+  api_key: APIkey,
+};
 
 export const fetchTrending = async () => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIkey}`
-  );
-  //const data = await response.json();
-  // return data.results;
-  return (await response.json()).results;
+  const response = await axios.get(`trending/movie/day`);
+
+  return response.data.results;
 };
 export const fetchSpecific = async query => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${APIkey}&language=en-US&page=1&include_adult=false&query=${query}`
+  const response = await axios.get(
+    `search/movie?page=1&include_adult=false&query=${query}`
   );
-  //   const data = await response.json();
-  //   return data.results;
-  return (await response.json()).results;
+  return response.data.results;
 };
 export const fetchMovieDetails = async movieId => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIkey}`
-  );
-
-  return await response.json();
+  const response = await axios.get(`movie/${movieId}`);
+  return response.data;
 };
 export const fetchCast = async movieId => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${APIkey}`
-  );
-  return (await response.json()).cast;
+  const response = await axios.get(`movie/${movieId}/credits`);
+  return response.data.cast;
 };
 export const fetchReviews = async movieId => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${APIkey}`
-  );
-  return (await response.json()).results;
+  const response = await axios.get(`movie/${movieId}/reviews`);
+  return response.data.results;
 };
